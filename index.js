69f70203cdc404f4486367c2b9294ed92b496b21
iarpittomar <arpittomar7740@gmail.com>
2021-05-30 00:31:23 +0530

    refactor: learn how to add dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
@@ -18,6 +19,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+// const slugs = dataObj.map((item) => slugify(item.productName, { lower: true }));
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
